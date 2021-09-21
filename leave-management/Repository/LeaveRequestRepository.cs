7902b7ef9c1d036385af93faa969418f00fc2a34
Jeremiah Ogunbor <jerryogunbor@gmail.com>
2021-09-21 09:51:16 +0100

    Add project files.

---
+++ leave-management/Repository/LeaveRequestRepository.cs
@@ -0,0 +1,59 @@
+﻿using leave_management.Contracts;
+using leave_management.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Repository
+{
+    public class LeaveRequestRepository : ILeaveRequestRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public LeaveRequestRepository(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public bool Create(LeaveRequest entity)
+        {
+            _db.LeaveRequests.Add(entity);
+            return Save();
+        }
+
+        public bool Delete(LeaveRequest entity)
+        {
+            _db.LeaveRequests.Remove(entity);
+            return Save();
+        }
+
+        public ICollection<LeaveRequest> FindAll()
+        {
+            var LeaveRequests = _db.LeaveRequests.ToList();
+            return LeaveRequests;
+        }
+
+        public LeaveRequest FindById(int id)
+        {
+            var LeaveRequest = _db.LeaveRequests.Find(id);
+            return LeaveRequest;
+        }
+
+        public ICollection<LeaveRequest> GetEmployeesByLeaveRequest(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Save()
+        {
+            var changes = _db.SaveChanges();
+            return changes > 0;
+        }
+
+        public bool Update(LeaveRequest entity)
+        {
+            _db.LeaveRequests.Update(entity);
+            return Save();
+        }
+    }
+}
